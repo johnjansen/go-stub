045154136f25685e45eed663154439bf40539d09
John Jansen <john.jansen@me.com>
2021-10-25 18:49:04 +1300

    Update setup.rb

--- scripts/setup.rb
+++ scripts/setup.rb
@@ -1,3 +1,21 @@
-if ARGV[0] == "new"
-    puts "hello"
+case ARGV[0]
+when "new"
+    # make the dir
+    Dir.mkdir ARGV[1]
+    # change dir
+    Dir.chdir ARGV[1]
+
+    # stub in all the directories we want
+    %w(cmd internal pkg vendor api web configs init build deployments test docs tools examples third_party githooks assets website).each do |dir|
+        Dir.mkdir(dir)
+    end
+
+    `go mod init ${ARGV[2]}`
+
+    # go back home
+    Dir.home
+else
+    puts <<-HELP
+        new project_name module_name
+    HELP
 end
